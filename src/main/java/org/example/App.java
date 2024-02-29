0ca6346f7ceb27768d590a4f0bf99e833765930a
janvipravinhaipatel <158079903+janvipravinhaipatel@users.noreply.github.com>
2024-02-29 11:48:21 -0500

    Initial commit failure

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String FizzBuzz(int a){
+        String Output = " ";
+        return Output;
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
