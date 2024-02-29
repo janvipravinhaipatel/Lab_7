1252d2a63caf55d381c3b345a7ec7346bf4acbdd
janvipravinhaipatel <158079903+janvipravinhaipatel@users.noreply.github.com>
2024-02-29 12:01:30 -0500

    Third commit pass

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,9 @@ public class App
 {
     public static String FizzBuzz(int a){
         String Output = " ";
-        if(a%3==0)
+        if (a%3==0 && a%5==0)
+            Output = "FizzBuzz";
+        else if(a%3==0)
             Output = "Fizz";
         else if (a%5==0)
             Output="Buzz";
